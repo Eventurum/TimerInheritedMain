c882a579a2710790c8fc470b4d64367310dc00d9
Ян Рабин <jan@rabin.ru>
2021-11-06 18:07:29 +0300

    commit

---
+++ lib/main.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:timer_inherited/src/timer_context.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StateWidget(
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.deepOrange,
+        ),
+        home: const MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({
+    Key? key,
+  }) : super(key: key);
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    final provider = TimerInheritedWidget.of(context);
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Таймер"),
+        ),
+        body: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                  height: 90,
+                  width: 90,
+                  margin: const EdgeInsets.all(10),
+                  child: FloatingActionButton(
+                    onPressed: () {
+                      provider?.restart();
+                    },
+                    child: const Icon(Icons.restart_alt),
+                  )),
+              Stack(
+                alignment: Alignment.center,
+                children: [
+                  Container(
+                      height: 90,
+                      width: 90,
+                      margin: const EdgeInsets.all(10),
+                      child: CircularProgressIndicator(
+                        value: provider?.percent,
+                        backgroundColor: Colors.blue[800],
+                        strokeWidth: 8,
+                      )),
+                  Positioned(
+                      child: Text(
+                    "${provider?.timeStr}",
+                    style: const TextStyle(
+                        fontWeight: FontWeight.bold, fontSize: 23),
+                    textAlign: TextAlign.center,
+                  ))
+                ],
+              ),
+              Container(
+                  height: 90,
+                  width: 90,
+                  margin: const EdgeInsets.all(10),
+                  child: FloatingActionButton(
+                    onPressed: () {
+                      provider!.isStart
+                          ? provider.pause()
+                          : provider.start(context);
+                    },
+                    child: provider!.isStart
+                        ? const Icon(Icons.pause)
+                        : const Icon(Icons.play_arrow),
+                  )),
+            ],
+          ),
+        ));
+  }
+}
